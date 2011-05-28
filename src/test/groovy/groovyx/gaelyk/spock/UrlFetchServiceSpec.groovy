41c2a17c4f8c8007fc702dc1d7ae47925185f05c
Marco Vermeulen <vermeulen.mp@gmail.com>
2011-05-28 13:16:16 +0100

    Renamed LocalServiceBound spec to UrlFetchServiceSpec. Also local.groovy to urlFetchTest.groovy.

--- src/test/groovy/groovyx/gaelyk/spock/UrlFetchServiceSpec.groovy
+++ src/test/groovy/groovyx/gaelyk/spock/UrlFetchServiceSpec.groovy
@@ -5,31 +5,21 @@ import com.google.appengine.tools.development.*
 import com.google.appengine.api.urlfetch.*
 import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 
-class LocalServicesBound extends GaelykUnitSpec {
+class UrlFetchServiceSpec extends GaelykUnitSpec {
 
 	def url = 'http://gaelyk.appspot.com'
 
 	def setup(){
-		groovlet 'local.groovy'
-		local.params.url = url
+		groovlet 'urlFetchTest.groovy'
+		urlFetchTest.params.url = url
 	}
-	
-	def "the mail service is present in the groovlet binding"(){
-		given: "the initialised groovlet"
-		
-		expect: "the mail service in the binding"
-		local.mail != null
-		local.mail instanceof com.google.appengine.api.mail.MailService
-	}
-	
-	//TODO: write specification that asserts mail was sent
 	
 	def "the url fetch service is present in the groovlet binding"(){
 		given: "the initialised groovlet"
 		
 		expect: "the urlfetch service is in the binding"
-		local.urlFetch != null
-		local.urlFetch instanceof com.google.appengine.api.urlfetch.URLFetchService
+		urlFetchTest.urlFetch != null
+		urlFetchTest.urlFetch instanceof com.google.appengine.api.urlfetch.URLFetchService
 	}
 	
 	def "the url fetch service is used from within the groovlet"(){
@@ -37,10 +27,10 @@ class LocalServicesBound extends GaelykUnitSpec {
 		def response = Mock(HTTPResponse)
 		
 		when: "the groovlet is invoked"
-		local.get()
+		urlFetchTest.get()
 		
 		then: "the url service should return a valid response"
 		urlFetch.fetch(new URL(url)) >> response
-		local.request.result == response
+		urlFetchTest.request.result == response
 	}
 }
